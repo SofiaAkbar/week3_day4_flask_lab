98af820ad259c22ad845fac1451ca724532be554
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-08-05 14:39:46 +0100

    MVP finished

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from models.event import *
 from models.event_planner import *
@@ -6,3 +6,18 @@ from models.event_planner import *
 @app.route('/events')
 def index():
     return render_template('index.html', title='Home', events=events)
+
+@app.route('/events', methods=['POST'])
+def add_event():
+    event_date = request.form['date']
+    event_name_of_event = request.form['name_of_event']
+    event_number_of_guests = request.form['number_of_guests']
+    event_room_location = request.form['room_location']
+    event_description = request.form['description']
+    new_event = Event(event_date, event_name_of_event, event_number_of_guests, event_room_location, event_description)
+    add_new_event(new_event)
+    return render_template('index.html', title='Home', events=events)
+
+@app.route('/events/new')
+def new_event():
+    return render_template('new_event.html', title='New Event')
--- models/event_planner.py
+++ models/event_planner.py
@@ -3,3 +3,6 @@ from models.event import *
 event1 = Event("10/08/21", "Wedding", 200, "Garden", "Tom and Sally marriage")
 event2 = Event("11/08/21", "Birthday Party", 30, "Piano Room", "Grandad's 90th")
 events = [event1, event2]
+
+def add_new_event(event):
+    events.append(event)
