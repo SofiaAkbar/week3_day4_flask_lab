988318f83bc7dd4491c6294a03cc5dfa97c75621
SofiaAkbar <sofiaakbar@hotmail.co.uk>
2021-08-05 15:11:34 +0100

    added date picker

--- models/event_planner.py
+++ models/event_planner.py
@@ -1,4 +1,5 @@
 from models.event import *
+import datetime
 
 event1 = Event("10/08/21", "Wedding", 200, "Garden", "Tom and Sally marriage")
 event2 = Event("11/08/21", "Birthday Party", 30, "Piano Room", "Grandad's 90th")
